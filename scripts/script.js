77feea7d830dd79813ae03a7a1ed154a4e66fa39
Jisun696 <istiakkashem35@gmail.com>
2024-03-03 22:14:17 +0600

    made the discussion all posts section mobile responsive

--- scripts/script.js
+++ scripts/script.js
@@ -39,19 +39,19 @@ const loadAllPosts = async () => {
 const displayAllPosts = (allPosts) => {
   allPosts.forEach((post) => {
     allPostsContainer.innerHTML += `
-    <div class="flex gap-4 bg-zinc-100 rounded-3xl p-10 shadow-xl">
+    <div class="flex flex-col md:flex-row gap-4 bg-zinc-100 rounded-3xl p-10 shadow-xl">
       <div id="author_img" class="w-[72px] h-[72px] bg-white rounded-2xl relative">
         <img src=${post.image} class="rounded-2xl" />
         <div class="w-[18.67px] h-[18.67px] ${post.isActive ? 'bg-emerald-500' : 'bg-red-500'}  rounded-[100px] border-2 border-white absolute right-0 top-0"></div>
       </div>
 
-      <div class="w-full">
+      <div class="md:w-full">
         <div>
           <span class="text-slate-900 text-opacity-80 text-sm font-medium font-inter mr-5"># ${post.category}</span>
           <span class="text-slate-900 text-opacity-80 text-sm font-medium font-inter">Author : ${post.author?.name}</span>
         </div>
         <h2 class="text-slate-900 text-xl font-bold font-mulish mt-3"> ${post.title} </h2>
-        <p class="mt-4 w-[569px] text-slate-900 text-opacity-60 text-base font-normal font-inter leading-relaxed">
+        <p class="mt-4 max-w-[569px] text-slate-900 text-opacity-60 text-base font-normal font-inter leading-relaxed">
           ${post.description}
         </p>
         <div class="mt-5 pt-6 border-t border-dashed border-slate-900 border-opacity-25 flex justify-between items-center">
